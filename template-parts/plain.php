e4cf0a2ba20485531addfa29fbd68d383d66876f
jcklpe <david@jackalope.tech>
2019-11-28 00:05:40 -0600

    fix plain php issue

--- template-parts/plain.php
+++ template-parts/plain.php
@@ -1,22 +1,23 @@
+<?php
 /*
 Template Name: Plain
 */
-<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
-    <header class="entry-header">
-        <?php
-        if (is_single()) :
-        //			the_title( '<h1 class="entry-title">', '</h1>' );
-        else :
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-        endif;
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> <header class="entry-header">
+    <?php
+    if (is_single()) :
+    //			the_title( '<h1 class="entry-title">', '</h1>' );
+    else :
+        the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+    endif;
 
-        if ('post' === get_post_type()) : ?>
-            <div class="entry-meta">
-                <?php jackalope_posted_on(); ?>
-            </div>
-            <!-- .entry-meta -->
-        <?php
-        endif; ?>
+    if ('post' === get_post_type()) : ?>
+        <div class="entry-meta">
+            <?php jackalope_posted_on(); ?>
+        </div>
+        <!-- .entry-meta -->
+    <?php
+    endif; ?>
     </header>
     <!-- .entry-header -->
 
